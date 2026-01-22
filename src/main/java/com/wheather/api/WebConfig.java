cb1d6b6fce1261c45a4993166badbd39b5ad3c30
pr4dyumn4 <pradyumnajekumar@gmail.com>
2026-01-22 17:46:59 +0530

    added configuration

---
+++ src/main/java/com/wheather/api/WebConfig.java
@@ -0,0 +1,15 @@
+package com.wheather.api;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+	public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // All endpoints
+                .allowedOriginPatterns("http://localhost:5173", "http://localhost:3000")  // Vite ports
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}

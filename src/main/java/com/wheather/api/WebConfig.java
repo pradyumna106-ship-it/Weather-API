dc7e40d48f3d56f9696a5c056b7ad92fc85ae698
pr4dyumn4 <pradyumnajekumar@gmail.com>
2026-02-18 14:35:18 +0530

    vercel origins allowed

--- src/main/java/com/wheather/api/WebConfig.java
+++ src/main/java/com/wheather/api/WebConfig.java
@@ -8,6 +8,7 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // All endpoints
                 .allowedOriginPatterns("http://localhost:5173", "http://localhost:3000")  // Vite ports
+                .allowedOriginPatterns("https://weather-app-theta-brown-28.vercel.app","https://weather-app-git-main-j-pradyumnas-projects.vercel.app","https://weather-qnmyl858e-j-pradyumnas-projects.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);

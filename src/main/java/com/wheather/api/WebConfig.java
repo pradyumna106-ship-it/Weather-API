771f54497c003f3b96225a4445b316d738488d42
pr4dyumn4 <pradyumnajekumar@gmail.com>
2026-02-18 15:47:09 +0530

    fixed origin pattern

--- src/main/java/com/wheather/api/WebConfig.java
+++ src/main/java/com/wheather/api/WebConfig.java
@@ -9,9 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")  // All endpoints
                 .allowedOriginPatterns("http://localhost:5173", "http://localhost:3000")  // Vite ports
                 .allowedOriginPatterns(
-                		"https://weather-app-theta-brown-28.vercel.app",
-                		"https://weather-app-git-main-j-pradyumnas-projects.vercel.app",
-                		"https://weather-qnmyl858e-j-pradyumnas-projects.vercel.app"
+                		"https://*.vercel.app"
                 		)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")

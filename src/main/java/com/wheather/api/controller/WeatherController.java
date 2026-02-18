c87770f12824cdcf8c6cd626131aefbea9a46815
pr4dyumn4 <pradyumnajekumar@gmail.com>
2026-02-18 15:57:27 +0530

    controller updated

--- src/main/java/com/wheather/api/controller/WeatherController.java
+++ src/main/java/com/wheather/api/controller/WeatherController.java
@@ -1,13 +1,14 @@
 package com.wheather.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wheather.api.service.WeatherService;
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/weather")
 public class WeatherController {
